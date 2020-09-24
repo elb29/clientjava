b9d9f35b7c63e8e184a997e3e9a1681a568a97ff
Etienne LE BIHAN <elebihan@IFREMER.FR>
2020-09-24 10:32:26 +0200

    first commit

---
+++ src/main/java/com/mycompany/testfrost/Connection.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testfrost;
+
+
+import de.fraunhofer.iosb.ilt.sta.ServiceFailureException;
+import de.fraunhofer.iosb.ilt.sta.model.EntityType;
+import de.fraunhofer.iosb.ilt.sta.model.Location;
+import de.fraunhofer.iosb.ilt.sta.model.Thing;
+import de.fraunhofer.iosb.ilt.sta.model.ext.EntityList;
+import de.fraunhofer.iosb.ilt.sta.service.SensorThingsService;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*; 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author elebihan
+ */
+public class Connection {
+    private URL url ;
+    private SensorThingsService service;
+    
+    
+    
+    public URL getUrl(){
+            return url;
+    }
+    
+    public void setUrl(String newURL){
+        try {
+            url = new URL(newURL);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public SensorThingsService getService(){
+        return service;
+    }
+    
+    public void setService(SensorThingsService serv){
+       service = serv;
+    }
+    
+    public Connection(String rootURL) throws MalformedURLException { 
+        url = new URL(rootURL);
+        service  = new SensorThingsService(url);
+    }
+    
+    public EntityList<Location> getlastLocations() throws ServiceFailureException{
+    	EntityList<Thing> allthings = service.things().query().list();
+    	
+    	EntityList<Location> lastLocations = new EntityList<Location>(EntityType.LOCATION);
+    	
+    	Iterator<Thing> iThings = allthings.fullIterator();
+		while (iThings.hasNext()) {
+		    Thing selectedThing = iThings.next();
+		    
+		    EntityList<Location> thingLoc = selectedThing.getLocations();
+		    		    
+		    if(!thingLoc.isEmpty()) {
+		    	Iterator<Location> locations = selectedThing.getLocations().fullIterator();
+		    	
+		    	Location lastLoc = locations.next();
+		    	
+		    	 lastLocations.add(lastLoc);
+		    }    		   
+		}
+    	
+    	return lastLocations;
+    }
+    	
+    
+}
+
+
---
+++ src/main/java/com/mycompany/testfrost/Main.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testfrost;
+
+import de.fraunhofer.iosb.ilt.sta.ServiceFailureException;
+import de.fraunhofer.iosb.ilt.sta.model.Thing;
+import de.fraunhofer.iosb.ilt.sta.model.ext.EntityList;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.util.Iterator;
+
+import javax.swing.*;
+/**
+ *
+ * @author elebihan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     * @throws ServiceFailureException 
+     */
+    public static void main(String[] args) throws MalformedURLException, ServiceFailureException {
+        // TODO code application logic here
+    	
+    	JFrame f=new JFrame();//creating instance of JFrame  
+    	
+    	JLabel l1;
+    	l1=new JLabel("Root service URL :");  
+    	l1.setBounds(130,50, 200,40); 
+    	f.add(l1);
+    	
+    	
+    	JTextField urlText=new JTextField();  
+        urlText.setBounds(130,100, 150,20); 
+        f.add(urlText);
+        
+    	JButton validateButton=new JButton("Validate");//creating instance of JButton  
+    	validateButton.setBounds(130,200,100, 40);//x axis, y axis, width, height  
+    	          
+    	validateButton.addActionListener(new ActionListener() { 
+    		  public void actionPerformed(ActionEvent e) { 
+    			    try {
+						Connection staConn = new Connection(urlText.getText());
+						
+						staConn.getlastLocations();
+						
+				    } catch (MalformedURLException | ServiceFailureException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+    			  }
+    	});
+    
+    	f.add(validateButton);//adding button in JFrame  
+    	          
+    	f.setSize(400,500);//400 width and 500 height  
+    	f.setLayout(null);//using no layout managers  
+    	f.setVisible(true);//making the frame visible  
+    	     
+    
+
+    }
+}
